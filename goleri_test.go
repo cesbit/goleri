59ecf0457d90067fa025946f6bdcf4ec0f11c4ed
Jeroen van der Heijden <jeroen@transceptor.technology>
2018-08-24 14:59:44 +0200

    Added test

--- goleri_test.go
+++ goleri_test.go
@@ -1,46 +1,39 @@
 package goleri
 
 import (
-	"fmt"
+	"reflect"
 	"regexp"
 	"testing"
 )
 
-func TestGoleri(t *testing.T) {
-	c := NewRef()
-	ni := NewRegex(0, regexp.MustCompile(`(?i)^n[ioa]`))
-
-	calc := NewPrio(
-		4,
-		ni,
-		NewSequence(0, THIS, NewTokens(0, "+ - / %"), THIS),
-		NewSequence(0, NewToken(0, "("), THIS, NewToken(0, ")")),
-	)
-
-	seq := NewSequence(
-		1,
-		calc,
-		c,
-		NewOptional(0, NewKeyword(0, "iris", false)),
-		NewList(0, ni, NewTokens(0, ", - ."), 1, 0, true))
-
-	c.Set(NewChoice(0, true, NewKeyword(0, "hoi", false), NewKeyword(0, "hallo", false)))
-
-	reKw := regexp.MustCompile(`^\w+`)
-
-	gr := NewGrammar(seq, reKw)
-
-	pr, err := gr.Parse("(Ni+(no - na)) hoi iris ni, ni.ni,")
-	if err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-	} else {
-		fmt.Printf("IsValid: %v\n", pr.IsValid())
-		fmt.Printf("Position: %v\n", pr.Pos())
-		fmt.Printf("Expecting: %v\n", pr.GetExpecting())
-		fmt.Printf("Tree: %v\n", pr.Tree())
-		if pr.IsValid() {
-			fmt.Printf("Tree: %v\n", pr.Tree().children[0])
-		}
+func assertEquals(t *testing.T, a interface{}, b interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(a, b) {
+		t.Fatalf("%s != %s", a, b)
 	}
+}
 
+func parse(t *testing.T, g *Grammar, s string) *Result {
+	t.Helper()
+	res, err := g.Parse(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return res
+}
+
+func TestKeyword(t *testing.T) {
+	hi := NewKeyword(0, "hi", false)
+	grammar := NewGrammar(hi, regexp.MustCompile(`^\w+`))
+
+	// assert statements
+	assertEquals(t, 0, hi.Gid())
+	assertEquals(t, false, hi.IsIgnCase())
+	assertEquals(t, true, parse(t, grammar, "hi").IsValid())
+	assertEquals(t, true, parse(t, grammar, " hi ").IsValid())
+	assertEquals(t, false, parse(t, grammar, "Hi").IsValid())
+	assertEquals(t, false, parse(t, grammar, "hello").IsValid())
+	assertEquals(t, "<Keyword gid:0 keyword:hi>", hi.String())
+	assertEquals(t, []Element{}, parse(t, grammar, "hi").GetExpecting())
+	assertEquals(t, []Element{hi}, parse(t, grammar, "").GetExpecting())
 }
