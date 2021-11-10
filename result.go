0a339a73659fb3d33b1e8dc2bd94dca359287692
Neal Cooper <neal.cooper@tcnbroadcasting.com>
2021-11-10 14:40:23 -0700

    keep track of string parsed in result, split string among child node when calling Tree()

--- result.go
+++ result.go
@@ -2,10 +2,11 @@ package goleri
 
 // Result is used as a Parse return value.
 type Result struct {
-	isValid bool
-	pos     int
-	expect  *expecting
-	tree    *Node
+	isValid    bool
+	pos        int
+	expect     *expecting
+	tree       *Node
+	expression string
 }
 
 // IsValid returns true when a parse result is valid.
@@ -20,4 +21,21 @@ func (r *Result) GetExpecting() []Element {
 }
 
 // Tree returns the node tree.
-func (r *Result) Tree() *Node { return r.tree }
+func (r *Result) Tree() *Node {
+	r.assignExpressionParts()
+	return r.tree
+}
+
+func (r *Result) assignExpressionParts() {
+	var walk func(n *Node)
+	walk = func(n *Node) {
+		if n == nil {
+			return
+		}
+		n.Data = r.expression[n.start:n.end]
+		for _, c := range n.children {
+			walk(c)
+		}
+	}
+	walk(r.tree)
+}
